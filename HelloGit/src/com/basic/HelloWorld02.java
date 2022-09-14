a4070edd819ea3e8a3626eafeb64738e34307a8c
anhhu <anhhu@DESKTOP-EFLBFUE>
2022-09-14 16:35:33 +0700

    commit from clone

--- HelloGit/src/com/basic/HelloWorld02.java
+++ HelloGit/src/com/basic/HelloWorld02.java
@@ -5,6 +5,7 @@ public class HelloWorld02 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello World 2 times");
+		System.out.println("Hello World from Clone");
 	}
 
 }
